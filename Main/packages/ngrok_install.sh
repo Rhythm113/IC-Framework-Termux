227c2725321b256aa3d8ebd290e9396ac7422b54
Mahbub Anam <53085722+Rhythm113@users.noreply.github.com>
2022-01-14 19:57:10 +0600

    Create ngrok_install.sh

---
+++ Main/packages/ngrok_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Ngrok Installer by IC-Termux"
+echo "Detecting Device CPU..."
+case `dpkg --print-architecture` in
+aarch64)
+    architectureURL="arm64" ;;
+arm)
+    architectureURL="arm" ;;
+armhf)
+    architectureURL="armhf" ;;
+amd64)
+    architectureURL="amd64" ;;
+i*86)
+    architectureURL="i386" ;;
+x86_64)
+    architectureURL="amd64" ;;
+*)
+    echo "unknown architecture"
+esac
+echo -e $g"Downloading..!"
+wget "https://github.com/AL-AlamySploit/Ngrok-Test/blob/master/ngrok-stable-linux-${architectureURL}.zip?raw=true" -O ngrok.zip
+unzip ngrok.zip
+cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
+chmod 700 /data/data/com.termux/files/usr/bin/ngrok
+rm ngrok ngrok.zip
+echo "Installed..."
